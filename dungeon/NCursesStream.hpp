b5fbc9ad9ae06b4adb94d30a12a15d6792ff9000
Sam Sparks <sam.sparks@tolling.telvent.com>
2017-03-15 12:15:59 -0500

    output working for multiple lines

--- dungeon/NCursesStream.hpp
+++ dungeon/NCursesStream.hpp
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <ncurses.h>
+#include <boost//algorithm/string.hpp>
 
 #include "Character.hpp"
 
@@ -31,10 +32,10 @@ class NCursesStream
     class Window
     {
     protected:
-        int mHeight; ///< height of the window
-        int mWidth; ///< width of the window
-        int mStartRow; ///< start row of the window (origin is top left)
-        int mStartCol; ///< start column of the window (origin is top left)
+        size_t mHeight; ///< height of the window
+        size_t mWidth; ///< width of the window
+        size_t mStartRow; ///< start row of the window (origin is top left)
+        size_t mStartCol; ///< start column of the window (origin is top left)
 
         WINDOW* mAddress; ///< reference to ncurses window controlled by this object
 
@@ -44,12 +45,12 @@ class NCursesStream
         /// \param width the width of the window, in characters
         /// \param start_row the start row of the window
         /// \param start_col the start column of the window
-        Window(int height, int width, int start_row, int start_col)
+        Window(size_t height, size_t width, size_t start_row, size_t start_col)
         : mHeight(height),
           mWidth(width),
           mStartRow(start_row),
           mStartCol(start_col),
-          mAddress( newwin(mHeight, mWidth, mStartRow, mStartCol) )
+          mAddress( newwin(static_cast<int>(mHeight), static_cast<int>(mWidth), static_cast<int>(mStartRow), static_cast<int>(mStartCol)) )
         {
             //std::cerr << "Window(" << height << "," << width << "," << start_row << "," << start_col << ")\n"; // XXX
         }
@@ -62,19 +63,19 @@ class NCursesStream
         }
 
         // accessor method
-        inline int Height() const { return mHeight; }
-        inline int Width() const { return mWidth; }
-        inline int StartRow() const { return mStartRow; }
-        inline int EndRow() const { return StartRow() + Height(); }
-        inline int StartColumn() const { return mStartCol; }
-        inline int EndColumn() const { return StartColumn() + Width(); }
+        inline size_t Height() const { return mHeight; }
+        inline size_t Width() const { return mWidth; }
+        inline size_t StartRow() const { return mStartRow; }
+        inline size_t EndRow() const { return StartRow() + Height(); }
+        inline size_t StartColumn() const { return mStartCol; }
+        inline size_t EndColumn() const { return StartColumn() + Width(); }
         inline void Refresh() { wrefresh(mAddress); }
     };
 
     /// \brief Window with a box on the outer edge
     struct BoxWindow : public Window
     {
-        BoxWindow(int height, int width, int start_row, int start_col)
+        BoxWindow(size_t height, size_t width, size_t start_row, size_t start_col)
         : Window(height, width, start_row, start_col)
         {
             box(mAddress, 0, 0);
@@ -84,10 +85,10 @@ class NCursesStream
     class InputWindow : public Window
     {
         const std::string mPrompt = "> ";
-        const int mMaxInputLength;
+        const size_t mMaxInputLength;
         
     public:
-        InputWindow(int height, int width, int start_row, int start_col)
+        InputWindow(size_t height, size_t width, size_t start_row, size_t start_col)
         : Window(height, width, start_row, start_col),
           mPrompt("> "),
           mMaxInputLength(Width() - mPrompt.length())
@@ -130,11 +131,10 @@ class NCursesStream
         }
     };
 
-    struct OutputWindow : public BoxWindow
+    class OutputWindow : public BoxWindow
     {
-
     public:
-        OutputWindow(int height, int width, int start_row, int start_col)
+        OutputWindow(size_t height, size_t width, size_t start_row, size_t start_col)
         : BoxWindow(height, width, start_row, start_col)
         {
             box(mAddress, 0, 0);
@@ -142,9 +142,41 @@ class NCursesStream
 
         void Write(const std::string& msg)
         {
-            // boost split on newline
-            // add to list
-            mvwprintw(mAddress, 1, 1, "%s", msg.c_str());
+        //std::cerr << "Msg: " << msg << '\n';// XXX
+            // put the message into the history, splitting lines that are longer than the window or have a newline
+
+            // split on newlines
+            std::vector<std::string> lines;
+            boost::split(lines, msg, boost::is_any_of("\r\n"));
+
+            // split long-lines into two lines
+            for(auto itr=lines.begin(); itr!=lines.end(); ++itr)
+            {
+                if( itr->size() > Width() - 2 )
+                {
+                    lines.insert(itr, itr->substr(0, Width()-2));
+                    itr->erase(Width()-2);
+                }
+            }
+
+            //TODO: handle messages longer than Height()-2 lines long
+            //TODO: add ability to scroll up and see history
+
+            if(lines.size() > Height()-2)
+            {
+                mvwprintw(mAddress, 1, 1, "ERROR: Message too long (%d lines)", lines.size());
+            }
+            else
+            {
+                for(size_t i=1; i<(Height()-2); ++i)
+                {
+                    wmove(mAddress, i, 1);
+                    if( lines.size() >= i )
+                        wprintw(mAddress, "%s", lines.at(i-1).c_str());
+                    wclrtoeol(mAddress);
+                }
+            }        
+            box(mAddress, 0, 0);
             wrefresh(mAddress);
         }
     };
@@ -157,7 +189,7 @@ class NCursesStream
     BoxWindow mOuterWindow; ///< outer window containing all game windows
 
 public:
-    NCursesStream(int display_height=30, int display_width=50, int output_window_height=5, int character_window_width=10)
+    NCursesStream(size_t display_height=30, size_t display_width=50, size_t output_window_height=5, size_t character_window_width=10)
     : mNCurses(),
       mDisplayWindow(display_height, display_width, 1, 1),
       mCharacterWindow(mDisplayWindow.Height(), character_window_width, mDisplayWindow.StartRow(), mDisplayWindow.EndColumn()+1),
